1e9761b405bc6211818db710927795cd069ade92
Patrick Stopper <patrick.stopper@outlook.de>
2020-12-16 13:52:01 +0100

    button zu submit

--- php/articleCard.php
+++ php/articleCard.php
@@ -12,7 +12,7 @@
     </div>
     <div class="card-footer">
     <small class="text-muted"><b><?= $row['price']?>€</b></small>
-    <button class="btn btn-info btn-sm" type="button" style="float:right;">In Warenkorb</button>
+    <button class="btn btn-info btn-sm" type="submit" style="float:right;">In Warenkorb</button>
     <button class="btn btn-light btn-sm" type="button" style="float:right;" data-toggle="popover" data-content="<?= $row['descr']?>" data-container="body" data-placement="top">Details</button>
     </div>
 </div>
