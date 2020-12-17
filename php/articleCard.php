93831bda8797bb4179cc92cf9370f5640d324038
Patrick Stopper <patrick.stopper@outlook.de>
2020-12-17 17:44:56 +0100

    grafischen fehler im shop korrigiert

--- php/articleCard.php
+++ php/articleCard.php
@@ -1,3 +1,5 @@
+
+
 <script>
     $( function( )
     {
@@ -11,12 +13,15 @@
         <h5 class="card-title"><?= $row['title']?></h5>
     </div>
     <div class="card-footer">
-    
-    <small class="text-muted"><b><?= $row['price']?>€</b></small>
-    <form method="post" action="addToCart.php">
-    <button class="btn btn-info btn-sm" style="float:right;" type="submit" name="warenkorbButton" onclick="alert('Produkt wurde in den Warenkorb hinzugefügt.');">In Warenkorb</button>
-    <input type="hidden" name="id" value="<?= $row['id']?>">
-    </form>
-    <button class="btn btn-light btn-sm" type="button" style="float:right;" data-toggle="popover" data-content="<?= $row['descr']?>" data-container="body" data-placement="top">Details</button>
+    <div >
+        
+        <form method="post" action="addToCart.php">
+        <small class="text-muted"><b><?= $row['price']?>€</b></small>
+        <button class="btn btn-info btn-sm" style="float:right;" type="submit" name="warenkorbButton" onclick="alert('Produkt wurde in den Warenkorb hinzugefügt.');">In Warenkorb</button>
+        <input type="hidden" name="id" value="<?= $row['id']?>">
+        <button class="btn btn-light btn-sm" type="button" style="float:right;" data-toggle="popover" data-content="<?= $row['descr']?>" data-container="body" data-placement="top">Details</button>
+        </form>
+        
+        </div>
     </div>
 </div>
