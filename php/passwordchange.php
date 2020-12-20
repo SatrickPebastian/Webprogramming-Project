10a61677e6c1735ec2f2aad789202af4ed164bdc
Svenja Bystrzinski <svenja@aby.de>
2020-12-20 22:26:26 +0100

    Regestrierung 2

--- php/passwordchange.php
+++ php/passwordchange.php
@@ -5,3 +5,38 @@ session_start();
         header("Location: ../login.html");
     }
 ?>
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <form>
+      <fieldset>
+        <legend>Passwort bestätigen</legend>
+        <label for="password1">Passwort:</label>
+        <input type="password" required id="password1" />
+  
+        <label for="password2">Passwort bestätigen:</label>
+        <input type="password" required id="password2" />
+
+        <button>Absenden</button>
+      </fieldset>
+    </form>
+    <script type="text/javascript">
+        var password1 = document.getElementById('password1');
+        var password2 = document.getElementById('password2');
+
+        var checkPasswordValidity = function() {
+            if (password1.value != password2.value) {
+                password2.setCustomValidity('Passwörter müssen übereinstimmen!');
+            } else {
+                password2.setCustomValidity('');
+            }        
+        };
+
+        password1.addEventListener('change', checkPasswordValidity);
+        password2.addEventListener('change', checkPasswordValidity);
+    </script>
+    </body>
+</html>
