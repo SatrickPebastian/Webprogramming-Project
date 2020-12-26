abc727a834d74f09755b03366d8304a4a3289686
Patrick Stopper <patrick.stopper@outlook.de>
2020-12-26 17:35:25 +0100

    online user optimiert

--- php/getOnline.php
+++ php/getOnline.php
@@ -14,15 +14,23 @@
       $sql = "SELECT username, status FROM user WHERE status = 'online';";
       $result = $webshopcon->query($sql);
 
+      $counter = 0;
+
       //Gibt Online User aus
         while($row = $result->fetch_assoc()){
-          if($row != null){
+          
+          if($row['status'] == 'online'){
             echo '<div class="container>"';
             echo '<p><i class="fas fa-circle" style="color:forestGreen;font-size:13px;"></i> '.$row['username'].'</p>';
             echo '</div>';
-          }else {
-            echo "Kein Nutzer online.";
+            $counter++;
           }
+        }
+
+        if($counter == 0){
+          echo '<div class="container">';
+          echo '<p>Kein Nutzer online.</p>';
+          echo '</div>';
         }
     
         //'<div class="container"><div class="row"><div class="col-0"><i class="fas fa-circle" style="color:green;font-size:13px;"></i></div><div class="col-2"><p class="float-left">'.$row['username'].'</p></div></div></div>';
