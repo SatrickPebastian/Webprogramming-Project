1c8c8b0d890a0ec9a722aad4f2ac8cdf2d7724d8
Patrick Stopper <patrick.stopper@outlook.de>
2021-03-13 23:28:06 +0100

    deleted title

--- hilfe.php
+++ hilfe.php
@@ -7,7 +7,7 @@
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Hilfe</title>
+     
 
      <link rel="icon" href="images/resourceImages/hai23.png">
 
